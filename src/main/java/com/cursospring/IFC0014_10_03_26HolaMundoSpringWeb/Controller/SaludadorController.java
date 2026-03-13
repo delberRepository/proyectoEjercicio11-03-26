d3911926379e7f5ef5c9e69de6c3b9c3f9b2f5ed
unknown <drfranky@hotmail.com>
2026-03-13 13:03:27 +0100

    Actualizo cambios 4

--- src/main/java/com/cursospring/IFC0014_10_03_26HolaMundoSpringWeb/Controller/SaludadorController.java
+++ src/main/java/com/cursospring/IFC0014_10_03_26HolaMundoSpringWeb/Controller/SaludadorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
   @RestController
-  @RequestMapping("/saludos")
+  @RequestMapping("/")
   public class SaludadorController {
 
     @Autowired
--- src/main/java/com/cursospring/IFC0014_10_03_26HolaMundoSpringWeb/Model/Saludo.java
+++ src/main/java/com/cursospring/IFC0014_10_03_26HolaMundoSpringWeb/Model/Saludo.java
@@ -2,5 +2,10 @@ package com.cursospring.IFC0014_10_03_26HolaMundoSpringWeb.Model;
 
 public record Saludo(String mensaje) {
 
-
+    @Override
+    public String toString() {
+        return "Saludo{" +
+                "mensaje='" + mensaje + '\'' +
+                '}';
+    }
 }
